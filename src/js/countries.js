5f0640b83eec5926f2a42b175d6453be154cd2ce
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-05-18 20:15:06 +0300

    saved refs

--- src/js/countries.js
+++ src/js/countries.js
@@ -1,12 +1,6 @@
 import { fetchCountries } from './fetchCountries.js';
 import debounce from 'lodash.debounce';
 import { makeMarkup, renderError } from './markup.js';
-// import countriesList from '../templates/country-list.hbs';
-// import updateCountryMarkup from '../templates/countries.hbs';
-
-// import { alert, error } from '@pnotify/core';
-// import '@pnotify/core/dist/PNotify.css';
-// import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = {
   input: document.querySelector('input'),
@@ -23,21 +17,3 @@ refs.input.addEventListener(
   }, 500),
 );
 export default refs;
-
-// function makeMarkup(data) {
-//   if (data.length > 1 && data.length <= 10) {
-//     refs.container.innerHTML = countriesList(data);
-//   } else if (data.length > 10) {
-//     alert({
-//       text: 'Too may matches found. Please enter a more specific query.',
-//     });
-//   } else {
-//     refs.container.innerHTML = data.map(country => updateCountryMarkup(country));
-//   }
-// }
-
-// function renderError() {
-//   error({
-//     text: 'Wrong entry! Try again',
-//   });
-// }
